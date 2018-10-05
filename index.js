37efe62771b7c8aec7812b1dccedf7a5fa337daa
snorrehu <snorre.hukkelaas@gmail.com>
2018-10-05 10:13:50 +0200

    Second commit

--- index.js
+++ index.js
@@ -2,37 +2,68 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var users = {};
+var users = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-//Whenever someone enters/refreshes the page:
+//Handle connection
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('Connection established.');
 
+  //Remove user on page close/refresh (to be updated..)
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+  	var name = getUserName(socket.id);
+  	removeUser(socket.id);
+  	io.emit('user-left', name);
+    console.log(name + ' disconnected with ID: ' + socket.id);
+    console.log("Number of chatters: " + users.length);
   });
 
+  //Handle chat message from client
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-    console.log('Message sent: ' + msg);
+    console.log(msg.user + " says: " + msg.message);
   });
 
-  socket.on('send-nickname', function(msg){
-  	users.push(msg);
-  	console.log(msg + " has entered the chatroom.");
-    console.log("All users: " + users);
-    io.emit('new-user', msg);
+  //Handle user info from client
+  socket.on('setSocketID', function(data){
+  	console.log("User info recieved from client! Name: " + data.nickName + ", ID: " + data.userID);
+  	users.push(data);
+  	console.log("Number of chatters: " + users.length);
+    io.emit('new-user', data.nickName);
   });
 
 });
 
+//Listen to port:
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
 //Send event to everyone:
 io.emit('some event', { for: 'everyone' });
+
+//Get user from ID:
+function getUserName(id){
+	var i;
+	var name;
+	for (i = 0; i < users.length; i++) { 
+    	if(users[i].userID){
+    		name = users[i].nickName;
+    	}
+	}
+	return name;
+}
+
+//Get user from ID:
+function removeUser(id){
+	var i;
+	for (i = 0; i < users.length; i++) { 
+    	if(users[i].userID===id){
+    		users.splice(i,1);
+    		console.log("index: " + i);
+    	}
+	}
+}
